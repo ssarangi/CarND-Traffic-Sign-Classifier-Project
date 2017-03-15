531b9301a8b94c6cab4631bc8e51a91fab84d38c
Satyajit Sarangi <satyajits@google.com>
2017-03-15 15:47:29 -0700

    added the code for generating tensors. Need to fix the inputs

--- nnbuilder.py
+++ nnbuilder.py
@@ -9,25 +9,26 @@ class Maxpool:
         self.strides = strides
 
     def get_tensor(self, inputs):
-        return tf.layers.max_pooling2d(inputs=inputs, self.size, self.strides)
+        return tf.layers.max_pooling2d(inputs, self.size, self.strides)
 
     def __str__(self):
-        return "Maxpool: (%s) -> (%s)" % (size, strides)
+        return "Maxpool: (%s) -> (%s)" % (self.size, self.strides)
 
     __repr__ = __str__
 
 class CNN:
-    def __init__(self, filters, kernel_size, strides, padding, activation_func):
+    def __init__(self, name, filters, kernel_size, strides, padding, activation_func):
+        self.name = name
         self.filters = filters
         self.kernel_size = kernel_size
-        self.stride = stride
+        self.strides = strides
         self.padding = padding
         self.activation_func = activation_func
         self.maxpool = None
-        self.connects_to = None
+        self.next = None
 
     def _activation_func(self):
-        if activation_func == "relu":
+        if self.activation_func == "relu":
             return tf.nn.relu
 
         return None
@@ -36,9 +37,9 @@ class CNN:
         tensor = tf.layers.conv2d(
             inputs=inputs,
             filters=self.filters,
-            kernel_size=kernel_size,
-            strides=strides,
-            padding=padding,
+            kernel_size=self.kernel_size,
+            strides=self.strides,
+            padding=self.padding,
             activation=self._activation_func()
         )
 
@@ -48,7 +49,7 @@ class CNN:
         return tensor
 
     def __str__(self):
-        return self.type
+        return self
 
     __repr__ = __str__
 
@@ -66,7 +67,7 @@ class Architecture:
         for key in arch_str:
             k = arch_str[key]
             if k['type'] == 'cnn':
-                node = CNN(k['filters'], k['filter_size'], k['stride'], k['padding'], k['activation_func'], k['maxpool'])
+                node = CNN(key, k['filters'], k['kernel_size'], k['strides'], k['padding'], k['activation_func'])
                 # Setup the Maxpool Node
                 node.maxpool = Maxpool(k['maxpool']['size'], k['maxpool']['strides'])
                 node_map[key] = node
@@ -78,7 +79,7 @@ class Architecture:
             src_node = node_map[key]
             if k['connects_to'] is not None:
                 target_node = node_map[k['connects_to']]
-                src_node.connects_to = target_node
+                src_node.next = target_node
 
         self._topological_sort()
 
@@ -89,15 +90,23 @@ class Architecture:
         # Do a topological sort to find out the first node we need
         pass
 
-    def generate_graph(features, labels):
-        entry_node = self._get_entry_point()
+    def generate_graph(self, features, labels):
+        entry_node = self._get_entry_node()
 
         # Now at this point we can recursively go through and find the tensor
-        
-
+        input = features
+        node = entry_node
+        tensor = None
+        while node is not None:
+            tensor = node.get_tensor(input)
+            input = tensor
+            node = node.next
 
 def main():
     arch = Architecture("lenet.json")
+    features = None
+    labels = None
+    arch.generate_graph(features, labels)
 
 if __name__ == "__main__":
     main()
